903b6cec476b69375c83f2a7cfdd99f865ad94e8
wangmta <wangmta@gmail.com>
2020-01-16 11:08:36 -0500

    feat: loader service

---
+++ src/app/reusable/loader/loader.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoaderService, LoaderState } from './loader.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit {
+  show = false;
+
+  private subscription: Subscription;
+
+  constructor(private loaderService: LoaderService) {}
+
+  ngOnInit() {
+    this.subscription = this.loaderService.loaderState.subscribe((state: LoaderState) => {
+      this.show = state.show;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
